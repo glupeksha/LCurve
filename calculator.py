4925289bfd7553da667dba9f2c596f06becbcb41
@Dream <kviduranga54@gmail.com>
2017-02-22 17:33:22 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,26 +1,22 @@
-import math
 def calculator():
     while True:
+        a=int(input("Enter number 1:"))
+        b=int(input("Enter number 2:"))
         c=raw_input("enter operator:")
-        if(c!="log"):
-            a=int(input("Enter number 1:"))
-            b=int(input("Enter number 2:"))
-            
-            
-            else:
-                print("invalid operator")
-        else:
-            a=int(input("Enter number:"))
-            log(a)
-            
-
-
-
-        
+        if(c=="+"):
+            add(a,b)
 def add(a,b):
     z=a+b
     print(z)
+    
+    
+def mul(num1,num2):
+    multflication = num1 * num2
+    return multflication
 
-    
-    
+def mod(num1,num2):
+    mod = num1 % num2
+    return mod
+
+
 calculator()
