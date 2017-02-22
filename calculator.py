5d11f5ba8cb4477eace4cf4c05084e696d99f72b
warunikaprimali <diupamali@gmail.com>
2017-02-22 17:30:28 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,13 +1,26 @@
+import math
 def calculator():
     while True:
-        a=int(input("Enter number 1:"))
-        b=int(input("Enter number 2:"))
         c=raw_input("enter operator:")
-        if(c=="+"):
-            add(a,b)
+        if(c!="log"):
+            a=int(input("Enter number 1:"))
+            b=int(input("Enter number 2:"))
+            
+            
+            else:
+                print("invalid operator")
+        else:
+            a=int(input("Enter number:"))
+            log(a)
+            
+
+
+
+        
 def add(a,b):
     z=a+b
     print(z)
 
-
+    
+    
 calculator()
