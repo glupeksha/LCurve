3a3b6b178b76e39a9d8f574c6b09a5f84ea61b12
warunikaprimali <diupamali@gmail.com>
2017-02-22 17:18:45 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,13 @@
+def calculator():
+    while True:
+        a=int(input("Enter number 1:"))
+        b=int(input("Enter number 2:"))
+        c=raw_input("enter operator:")
+        if(c=="+"):
+            add(a,b)
+def add(a,b):
+    z=a+b
+    print(z)
 
+
+calculator()
