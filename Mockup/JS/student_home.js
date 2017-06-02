646ac6ea2b5ace256cee820da5cb3e50fe19523f
Upeksha Liyanage <19upeksha96@gmail.com>
2017-06-02 13:37:05 +0530

    dummy data
    dummy data for homework

--- Mockup/JS/student_home.js
+++ Mockup/JS/student_home.js
@@ -1,4 +1,17 @@
-
+/*
 $(document).on("click",".sidebar-toggle",function(){
     $(".wrapper").toggleClass("toggled");
+});*/
+$(document).ready(function(){
+  var obj = JSON.parse('{ "subject":"John", "task_title":"Integration & Differentiation", "progress":"Done"}');
+  var homeworks = [
+    { "subject":"Maths", "task_title":"Geometry", "progress":"Not Done"},
+    { "subject":"History", "task_title":"Revolution", "progress":"Not Done"},
+    { "subject":"Science", "task_title":"Blood Circular System", "progress":"Not Done"}
+  ];
+    var str="";
+    for (var i=0; i < homeworks.length; i = i + 1){
+      str=str+"<li>"+JSON.parse(JSON.stringify(homeworks[i])).task_title+"</li>"
+    }
+    $("#homework_list").html(str);
 });
