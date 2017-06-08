8c24c5e9df405d3aa9e550ea071b5ce2a384790b
Upeksha Liyanage <19upeksha96@gmail.com>
2017-06-08 15:26:01 +0530

    toggle view more

--- Mockup/JS/student_home.js
+++ Mockup/JS/student_home.js
@@ -3,6 +3,7 @@ $(document).on("click",".sidebar-toggle",function(){
     $(".wrapper").toggleClass("toggled");
 });*/
 $(document).ready(function(){
+  var cr_flag=0;
   var obj = JSON.parse('{ "subject":"John", "task_title":"Integration & Differentiation", "progress":"Done"}');
   var homeworks = [
     { "subject":"Maths", "task_title":"Geometry", "progress":"Not Done"},
@@ -20,8 +21,24 @@ $(document).ready(function(){
     }
     $("#homework_task").html(taskList);
     $("#homework_subjects").html(subjectList);
-});
+    $(".slider").click(
+      function(){
+        if(cr_flag==0){
+          cr_flag=1;
+          var realHeight=$('#homework')[0].scrollHeight+10;
+          $("#homework").css({
 
-$(".viewmore").click(function(){
-  $("#homework").css({"height": 400});
-});
+            '-webkit-transition': 'height .4s',
+            "height":realHeight
+          });
+        }else{
+          cr_flag=0;
+          $("#homework").css({
+
+            '-webkit-transition': 'height .4s',
+            "height": '150px'
+          });
+        }
+
+      });
+    });
