cd1d942e2fa5f9de4eb9e7f57b2a6d1a2644685f
Upeksha Liyanage <19upeksha96@gmail.com>
2017-06-08 16:10:14 +0530

    done list
    icon changed

--- Mockup/JS/student_home.js
+++ Mockup/JS/student_home.js
@@ -6,21 +6,29 @@ $(document).ready(function(){
   var cr_flag=0;
   var obj = JSON.parse('{ "subject":"John", "task_title":"Integration & Differentiation", "progress":"Done"}');
   var homeworks = [
-    { "subject":"Maths", "task_title":"Geometry", "progress":"Not Done"},
-    { "subject":"History", "task_title":"Revolution", "progress":"Not Done"},
-    { "subject":"Science", "task_title":"Blood Circular System", "progress":"Not Done"},
-    { "subject":"Science", "task_title":"Blood Circular System", "progress":"Not Done"},
-    { "subject":"Science", "task_title":"Blood Circular System", "progress":"Not Done"},
-    { "subject":"Science", "task_title":"Blood Circular System", "progress":"Not Done"}
+    { "subject":"Maths", "task_title":"Geometry", "progress":0},
+    { "subject":"History", "task_title":"Revolution", "progress":1},
+    { "subject":"Science", "task_title":"Blood Circular System", "progress":1},
+    { "subject":"Science", "task_title":"Blood Circular System", "progress":0},
+    { "subject":"Science", "task_title":"Blood Circular System", "progress":0},
+    { "subject":"Science", "task_title":"Blood Circular System", "progress":0}
   ];
     var taskList="";
     var subjectList="";
+    var doneList="";
     for (var i=0; i < homeworks.length; i = i + 1){
       taskList=taskList+"<li>"+JSON.parse(JSON.stringify(homeworks[i])).task_title+"</li>";
       subjectList=subjectList+"<li>"+JSON.parse(JSON.stringify(homeworks[i])).subject+"</li>";
+      if(JSON.parse(JSON.stringify(homeworks[i])).progress==1){
+        doneList=doneList+"<li><i class=\"fa fa-check-square-o\" aria-hidden=\"true\"></i></li>";
+      }else{
+        doneList=doneList+"<li><i class=\"fa fa-square-o\" aria-hidden=\"true\" ></i></li>";
+      }
+
     }
     $("#homework_task").html(taskList);
     $("#homework_subjects").html(subjectList);
+    $("#homework_done").html(doneList);
     $(".slider").click(
       function(){
         if(cr_flag==0){
