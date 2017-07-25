317b95078ef6d6f74f054e30ddb4fc92c03a4db8
Upeksha Liyanage <19upeksha96@gmail.com>
2017-07-26 00:50:21 +0530

    homework check through and notes

--- Mockup/JS/student_home.js
+++ Mockup/JS/student_home.js
@@ -4,12 +4,10 @@ $(document).ready(function(){
   //dummy data
   var avatar_url="https://scontent.fcmb2-1.fna.fbcdn.net/v/t1.0-9/17264485_1863589770546994_4450096234249845576_n.jpg?oh=2689c09530b7fd92800d66f964df2ce2&oe=59E6CCF0"
   var homeworks = [
-    { "subject":"Maths", "task_title":"Geometry", "progress":0},
-    { "subject":"History", "task_title":"Revolution", "progress":1},
-    { "subject":"Science", "task_title":"Blood Circular System", "progress":1},
-    { "subject":"English", "task_title":"Essay", "progress":0},
-    { "subject":"Science", "task_title":"Blood Circular System", "progress":0},
-    { "subject":"Science", "task_title":"Blood Circular System", "progress":0}
+    { "subject":"Maths","colour":"purple", "homework":[{"task_title":"1234567890123456789012345678901234567890", "progress":0}]},
+    { "subject":"History","colour":"yellow", "homework":[{"task_title":"Revolution", "progress":1}]},
+    { "subject":"Science","colour":"pink", "homework":[{"task_title":"Blood Circular System", "progress":1},{"task_title":"Digestive System", "progress":0}]},
+    { "subject":"English", "colour":"green", "homework":[{"task_title":"Essay", "progress":0}]}
   ];
   var notifications_sc = [
     { "Society":"Camera Club", "notification":"Photographers will be selected fo School proze giving on 7th October", "expired":0},
@@ -26,24 +24,38 @@ $(document).ready(function(){
   //Set homework data
   var total=0;
   var task_remaining=0;
-  var panels_cr="<div  class=\"panel-group\">";
+  var panels_cr="<div  class=\"col-lg-12 panel-group\">";
+
   for (var i=0; i < homeworks.length; i = i + 1){
-    total=total+1;
-    if(JSON.parse(JSON.stringify(homeworks[i])).progress==1){
-      panels_cr=panels_cr+"<div class=\"panel panel-success changepanel\">";
-      panels_cr=panels_cr+"<div class=\"panel-heading\">"+JSON.parse(JSON.stringify(homeworks[i])).subject+"</div>";
-      panels_cr=panels_cr+"<div class=\"panel-body\"><p class=\"cr_panel_title\">"+JSON.parse(JSON.stringify(homeworks[i])).task_title+"</p>";
-      panels_cr=panels_cr+"<i class=\"fa fa-check-square-o cr_panel_check\" aria-hidden=\"true\"></i>";
-    }else{
-      panels_cr=panels_cr+"<div class=\"panel panel-info changepanel\">";
-      panels_cr=panels_cr+"<div class=\"panel-heading\">"+JSON.parse(JSON.stringify(homeworks[i])).subject+"</div>";
-      panels_cr=panels_cr+"<div class=\"panel-body\"><p class=\"cr_panel_title\">"+JSON.parse(JSON.stringify(homeworks[i])).task_title+"</p>";
-      task_remaining=task_remaining+1;
-      panels_cr=panels_cr+"<i class=\"fa fa-square-o cr_panel_check\" aria-hidden=\"true\" ></i>";
+    panels_cr=panels_cr+"<div class=\"col-lg-4\"> ";
+    panels_cr=panels_cr+"<div class=\"panel panel-success "+homeworks[i].colour+"border changepanel MarginPanels\">";
+    panels_cr=panels_cr+"<div class=\"panel-heading "+homeworks[i].colour+" panel-heading-custom\">"+homeworks[i].subject+"</div>";
+    panels_cr=panels_cr+"<div class=\"panel-body\">";
+    panels_cr=panels_cr+"<form class=\"ac-custom ac-list\" autocomplete=\"off\">";
+    panels_cr=panels_cr+"<ul>";
+    for(var j=0;j<homeworks[i].homework.length;j=j+1){
+      total=total+1;
+      panels_cr=panels_cr+"<li>";
+      if(homeworks[i].homework[j].progress==1){
+        panels_cr=panels_cr+"<input id=\""+i+"cb"+j+"\" name=\""+i+"cb"+j+"\" type=\"checkbox\" class=\"cr_check\" checked>";
+        panels_cr=panels_cr+"<label for=\""+i+"cb"+j+"\" >"+homeworks[i].homework[j].task_title+"</label>";
+        panels_cr=panels_cr+"<svg viewBox=\"0 0 300 100\" preserveAspectRatio=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M1.986,8.91c41.704,4.081,83.952,5.822,125.737,2.867 c17.086-1.208,34.157-0.601,51.257-0.778c21.354-0.223,42.706-1.024,64.056-1.33c18.188-0.261,36.436,0.571,54.609,0.571\" style=\"stroke-dasharray: 295.877, 295.877; stroke-dashoffset: 0; transition: stroke-dashoffset 0.3s ease-in-out 0s;\"></path><path d=\"M3.954,25.923c9.888,0.045,19.725-0.905,29.602-1.432 c16.87-0.897,33.825-0.171,50.658-2.273c14.924-1.866,29.906-1.407,44.874-1.936c19.9-0.705,39.692-0.887,59.586,0.45 c35.896,2.407,71.665-1.062,107.539-1.188\" style=\"stroke-dasharray: 292.511, 292.511; stroke-dashoffset: 0; transition: stroke-dashoffset 0.3s ease-in-out 0.3s;\"></path></svg>";
+
+        panels_cr=panels_cr+"<svg viewBox=\"0 0 300 100\" preserveAspectRatio=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M1.986,8.91c41.704,4.081,83.952,5.822,125.737,2.867 c17.086-1.208,34.157-0.601,51.257-0.778c21.354-0.223,42.706-1.024,64.056-1.33c18.188-0.261,36.436,0.571,54.609,0.571\" style=\"stroke-dasharray: 295.877, 295.877; stroke-dashoffset: 0; transition: stroke-dashoffset 0.3s ease-in-out 0s;\"></path><path d=\"M3.954,25.923c9.888,0.045,19.725-0.905,29.602-1.432 c16.87-0.897,33.825-0.171,50.658-2.273c14.924-1.866,29.906-1.407,44.874-1.936c19.9-0.705,39.692-0.887,59.586,0.45 c35.896,2.407,71.665-1.062,107.539-1.188\" style=\"stroke-dasharray: 292.511, 292.511; stroke-dashoffset: 0; transition: stroke-dashoffset 0.3s ease-in-out 0.3s;\"></path></svg>";
+      }else{
+        panels_cr=panels_cr+"<input id=\""+i+"cb"+j+"\" name=\""+i+"cb"+j+"\" type=\"checkbox\" class=\"cr_check\">";
+        panels_cr=panels_cr+"<label for=\""+i+"cb"+j+"\" >"+homeworks[i].homework[j].task_title+"</label>";
+        task_remaining=task_remaining+1;
+      }
+      panels_cr=panels_cr+"</li>";
     }
+    panels_cr=panels_cr+"</ul>";
+    panels_cr=panels_cr+"</form>";
+    panels_cr=panels_cr+"</div>";
     panels_cr=panels_cr+"</div>";
     panels_cr=panels_cr+"</div>";
   }
+
   panels_cr=panels_cr+"</div>";
   if(task_remaining==0){
     $("#task_remaining").html("All Done");
@@ -56,14 +68,13 @@ $(document).ready(function(){
   //Set Society Anouncement Data
   var panels_sc="<div  class=\"panel-group\">";
   for (var i=0; i < notifications_sc.length; i = i + 1){
-    total=total+1;
     panels_sc=panels_sc+"<div class=\"panel panel-info\">";
     if(i%2==1){
-      panels_sc=panels_sc+"<div class=\"panel-heading\">"+JSON.parse(JSON.stringify(notifications_sc[i])).Society+"</div>";
-      panels_sc=panels_sc+"<div class=\"panel-body\">"+JSON.parse(JSON.stringify(notifications_sc[i])).notification+"</div>";
+      panels_sc=panels_sc+"<div class=\"panel-heading\">"+notifications_sc[i].Society+"</div>";
+      panels_sc=panels_sc+"<div class=\"panel-body\">"+notifications_sc[i].notification+"</div>";
     }else{
-      panels_sc=panels_sc+"<div class=\"panel-heading\">"+JSON.parse(JSON.stringify(notifications_sc[i])).Society+"</div>";
-      panels_sc=panels_sc+"<div class=\"panel-body\">"+JSON.parse(JSON.stringify(notifications_sc[i])).notification+"</div>";
+      panels_sc=panels_sc+"<div class=\"panel-heading\">"+notifications_sc[i].Society+"</div>";
+      panels_sc=panels_sc+"<div class=\"panel-body\">"+notifications_sc[i].notification+"</div>";
     }
     panels_sc=panels_sc+"</div>";
   }
@@ -73,23 +84,18 @@ $(document).ready(function(){
 
 
   //Homework Marker
-    $('.cr_panel_check').click(function(){
-      if($(this).hasClass("fa-check-square-o")){
-        task_remaining=task_remaining+1;
-        $(this).parentsUntil(".panel-group").filter(".changepanel").removeClass("panel-success");
-        $(this).parentsUntil(".panel-group").filter(".changepanel").addClass("panel-info");
-      }else{
+    $('.cr_check').click(function(){
+
+      if(this.checked){
         task_remaining=task_remaining-1;
-        $(this).parentsUntil(".panel-group").filter(".changepanel").removeClass("panel-info");
-        $(this).parentsUntil(".panel-group").filter(".changepanel").addClass("panel-success");
+      }else{
+        task_remaining=task_remaining+1;
       }
       if(task_remaining==0){
         $("#task_remaining").html("All Done");
       }else{
         $("#task_remaining").html(task_remaining+"/"+total);
       }
-
-      $(this).toggleClass('fa fa-check-square-o fa fa-square-o')
     });
 
     //Homework pane Slider
@@ -132,6 +138,11 @@ $(document).ready(function(){
 
     });
 
-    //Overflow of the DashBoard
+    //Homework Dashboard list
+    var script = document.createElement('script');
+    script.src = "js/svgcheckbx.js";
+    script.async = true;
+    document.head.appendChild(script);
+
 
 });
