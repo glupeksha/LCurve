e91bbf19447301138dfb3861b114655996a3d875
Upeksha Liyanage <19upeksha96@gmail.com>
2017-07-26 18:08:21 +0530

    homework dashboard complete

--- Mockup/JS/student_home.js
+++ Mockup/JS/student_home.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   var avatar_url="https://scontent.fcmb2-1.fna.fbcdn.net/v/t1.0-9/17264485_1863589770546994_4450096234249845576_n.jpg?oh=2689c09530b7fd92800d66f964df2ce2&oe=59E6CCF0"
   var homeworks = [
     { "subject":"Maths","colour":"purple", "homework":[{"task_title":"Solve Geometry questions in page 24, 25 and page 30", "progress":1}]},
-    { "subject":"History","colour":"yellow", "homework":[{"task_title":"Write a chapter on how french Revolution started and causes for it", "progress":0}]},
+    { "subject":"History","colour":"yellow", "homework":[{"task_title":"Write a paragraph on how french Revolution started and causes for it. Write your opinion about the king and the queens reaction.", "progress":0}]},
     { "subject":"Science","colour":"pink", "homework":[{"task_title":"Blood Circular System", "progress":1},{"task_title":"Digestive System", "progress":0}]},
     { "subject":"English", "colour":"green", "homework":[{"task_title":"Essay", "progress":0}]}
   ];
@@ -40,10 +40,6 @@ $(document).ready(function(){
         panels_cr=panels_cr+"<input id=\""+i+"cb"+j+"\" name=\""+i+"cb"+j+"\" type=\"checkbox\" class=\"cr_check\" checked>";
         panels_cr=panels_cr+"<label for=\""+i+"cb"+j+"\" >"+homeworks[i].homework[j].task_title+"</label>";
         panels_cr=panels_cr+"<svg viewBox=\"0 0 300 100\" preserveAspectRatio=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M1.986,8.91c41.704,4.081,83.952,5.822,125.737,2.867 c17.086-1.208,34.157-0.601,51.257-0.778c21.354-0.223,42.706-1.024,64.056-1.33c18.188-0.261,36.436,0.571,54.609,0.571\" style=\"stroke-dasharray: 295.877, 295.877; stroke-dashoffset: 0; transition: stroke-dashoffset 0.3s ease-in-out 0s;\"></path><path d=\"M3.954,25.923c9.888,0.045,19.725-0.905,29.602-1.432 c16.87-0.897,33.825-0.171,50.658-2.273c14.924-1.866,29.906-1.407,44.874-1.936c19.9-0.705,39.692-0.887,59.586,0.45 c35.896,2.407,71.665-1.062,107.539-1.188\" style=\"stroke-dasharray: 292.511, 292.511; stroke-dashoffset: 0; transition: stroke-dashoffset 0.3s ease-in-out 0.3s;\"></path></svg>";
-        if(homeworks[i].homework[j].task_title.length>50){
-          panels_cr=panels_cr+"<svg class=\"nextLine\" viewBox=\"0 0 300 100\" preserveAspectRatio=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M1.986,8.91c41.704,4.081,83.952,5.822,125.737,2.867 c17.086-1.208,34.157-0.601,51.257-0.778c21.354-0.223,42.706-1.024,64.056-1.33c18.188-0.261,36.436,0.571,54.609,0.571\" style=\"stroke-dasharray: 295.877, 295.877; stroke-dashoffset: 0; transition: stroke-dashoffset 0.3s ease-in-out 0s;\"></path><path d=\"M3.954,25.923c9.888,0.045,19.725-0.905,29.602-1.432 c16.87-0.897,33.825-0.171,50.658-2.273c14.924-1.866,29.906-1.407,44.874-1.936c19.9-0.705,39.692-0.887,59.586,0.45 c35.896,2.407,71.665-1.062,107.539-1.188\" style=\"stroke-dasharray: 292.511, 292.511; stroke-dashoffset: 0; transition: stroke-dashoffset 0.3s ease-in-out 0.3s;\"></path></svg>";
-        }
-
       }else{
         panels_cr=panels_cr+"<input id=\""+i+"cb"+j+"\" name=\""+i+"cb"+j+"\" type=\"checkbox\" class=\"cr_check\">";
         panels_cr=panels_cr+"<label for=\""+i+"cb"+j+"\" >"+homeworks[i].homework[j].task_title+"</label>";
@@ -60,6 +56,7 @@ $(document).ready(function(){
 
   if(task_remaining==0){
     $("#task_remaining").html("All Done");
+
   }else{
     $("#task_remaining").html(task_remaining+"/"+total);
   }
@@ -86,12 +83,11 @@ $(document).ready(function(){
 
   //Homework Marker
     $('.cr_check').click(function(){
-
       if(this.checked){
         task_remaining=task_remaining-1;
-        
       }else{
         task_remaining=task_remaining+1;
+        $(this).next().find(".nextLine").remove();
       }
       if(task_remaining==0){
         $("#task_remaining").html("All Done");
--- Mockup/js/svgcheckbx.js
+++ Mockup/js/svgcheckbx.js
@@ -50,9 +50,7 @@ if( document.createElement('svg').getAttributeNS ) {
 
 		el.addEventListener( 'change', function() {
 			if( el.checked ) {
-
-				draw( el, type );
-
+				draw(el,type);
 
 			}
 			else {
@@ -107,53 +105,7 @@ if( document.createElement('svg').getAttributeNS ) {
 			path.style.strokeDashoffset = '0';
 		}
 	}
-	function drawNext( el, type ) {
-		var paths = [], pathDef,
-			animDef,
-			svg = el.parentNode.querySelector( 'svg' );
 
-		switch( type ) {
-			case 'cross': pathDef = pathDefs.cross; animDef = animDefs.cross; break;
-			case 'fill': pathDef = pathDefs.fill; animDef = animDefs.fill; break;
-			case 'checkmark': pathDef = pathDefs.checkmark; animDef = animDefs.checkmark; break;
-			case 'circle': pathDef = pathDefs.circle; animDef = animDefs.circle; break;
-			case 'boxfill': pathDef = pathDefs.boxfill; animDef = animDefs.boxfill; break;
-			case 'swirl': pathDef = pathDefs.swirl; animDef = animDefs.swirl; break;
-			case 'diagonal': pathDef = pathDefs.diagonal; animDef = animDefs.diagonal; break;
-			case 'list': pathDef = pathDefs.list; animDef = animDefs.list; break;
-		};
-
-		paths.push( document.createElementNS('http://www.w3.org/2000/svg', 'path' ) );
-
-		if( type === 'cross' || type === 'list' ) {
-			paths.push( document.createElementNS('http://www.w3.org/2000/svg', 'path' ) );
-		}
-
-		for( var i = 0, len = paths.length; i < len; ++i ) {
-			var path = paths[i];
-			svg.setAttribute("class", "nextLine");
-			svg.appendChild( path );
-
-			path.setAttributeNS( null, 'd', pathDef[i] );
-
-			var length = path.getTotalLength();
-			// Clear any previous transition
-			//path.style.transition = path.style.WebkitTransition = path.style.MozTransition = 'none';
-			// Set up the starting positions
-			path.style.strokeDasharray = length + ' ' + length;
-			if( i === 0 ) {
-				path.style.strokeDashoffset = Math.floor( length ) - 1;
-			}
-			else path.style.strokeDashoffset = length;
-			// Trigger a layout so styles are calculated & the browser
-			// picks up the starting position before animating
-			path.getBoundingClientRect();
-			// Define our transition
-			path.style.transition = path.style.WebkitTransition = path.style.MozTransition  = 'stroke-dashoffset ' + animDef.speed + 's ' + animDef.easing + ' ' + i * animDef.speed + 's';
-			// Go!
-			path.style.strokeDashoffset = '0';
-		}
-	}
 	function reset( el ) {
 		Array.prototype.slice.call( el.parentNode.querySelectorAll( 'svg > path' ) ).forEach( function( el ) { el.parentNode.removeChild( el ); } );
 	}
