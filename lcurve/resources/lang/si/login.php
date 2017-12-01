961ae2b0c5d4794e4fdddf5159fe7f60040aea46
SANJ0094 <pmts12344321@gmail.com>
2017-12-01 15:19:23 +0530

    localization
    part 1

---
+++ lcurve/resources/lang/si/login.php
@@ -0,0 +1,35 @@
+<?php  
+
+	return [
+
+		'email'=>'විද්‍යුත් තැපෑල',
+		'password'=>'මුරපදය',
+		'remember'=>'මාව මතක තබා ගන්න',
+		'loginbutton'=>'පද්ධතියට ඇතුළුවීම',
+		'forgot'=>'මුරපදය අමතක වුනාද?',
+		'loginHeading'=>'පද්ධතියට ඇතුළුවීම',
+		
+
+		//register page
+		
+		'Register'=>'ලියාපදිංචි වීම',
+		'Name'=>'නම',
+		'passwordReg'=>'මුරපදය',
+		'confirmPassword'=>'මුරපදය තහවුරු කරන්න',
+		'registerProceed'=>'ලියාපදිංචි වීම',
+
+		// email reset password
+
+		'resetPassword'=>'මුරපදය වෙනස් කිරීම',
+		'resetLink'=>'මුරපදය වෙනස් කිරීමට විද්‍යුත් තැපැල යවන්න',
+
+		//navbar
+		'home'=>'මුල් පිටුව',
+		'classRoom'=>'පංති කාමරය',
+		'societies'=>'පාසැල් සමිති',
+		'sports'=>'ක්‍රීඩා',
+		'profile'=>'මගේ ගිණුම',
+		'logout'=>'පද්ධතියෙන් ඉවත්වන්න',
+
+
+	];
