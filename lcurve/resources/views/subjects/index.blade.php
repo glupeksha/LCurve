7b4dc66273599cf08ad44b70904da5839e4d6319
warunikaprimali <warunikaprimali94@gmail.com>
2017-12-09 21:56:31 +0530

    instnt subject.index page copy & paste

--- lcurve/resources/views/subjects/index.blade.php
+++ lcurve/resources/views/subjects/index.blade.php
@@ -1,32 +1,69 @@
+@extends('layouts.app')
+@section('dash-left')
+
 <div class="row">
-
-	<div >
-    <div class="panel panel-default">
-        <div class="panel-heading">
-          <h3>Subjects</h3>
-          <hr class="hr_style">
-          <div class="panel-body">
-            @foreach ($subjects as $subject)
+    <div class="col-md-10 col-md-offset-1">
+        <div class="panel panel-default ">
+            <div class="panel-heading">
+                <h3>Subjects</h3>
+            </div>
 
 
+            <div class="panel-body">
             @foreach ($subjects as $subject)
                 <div class="panel-body panel-heading">
-                <div class="col-lg-1"></div>
-                        <a href="{{ route('subjects.show', $subject->id ) }}" style="font-family:sans-serif;"><b>
-                          {{  $subject->name }}
-                        </b><br>
-                        </a>
+                <div class="row">
+                <!--society name load start-->
+                    <div class="col-lg-8">
+                    <div class="col-lg-2"></div>
+                        {{--<button class="buttonstyles">--}}
+                              <a href="{{ route('subjects.show', $subject->id ) }}"><b>{{ $subject->name }}</b></a>
+                        {{--</button>--}}
+
+                    </div>
+                    <!--edit delete buttons start-->
+                    <div class="col-lg-4">
+                            {!! Form::open(['method' => 'DELETE','onsubmit' => 'return confirm("Are you sure?")','route' => ['subjects.destroy', $subject->id] ]) !!}                 
+                                @can('Edit Sport')
+                                <a href="{{ route('subjects.edit', $subject->id) }}" class="btn btn-success" role="button">Edit</a>
+                                @endcan
+                                
+                                @can('Delete Sport')                               
+                                  {!! Form::submit('Delete',['class'=>'btn btn-success']) !!}
+                                @endcan
+                            {!! Form::close() !!}
+                    
+                    </div>
+                    <!--edit delete buttons end-->
+                <!--society name load end-->
+                     <div class="col-lg-4">
+                      {!! Form::open(['method' => 'DELETE','onsubmit' => 'return confirm("Are you sure?")','route' => ['subjects.destroy', $subject->id] ]) !!}
+
+
+                <!--edit and delete button start-->
+                        @can('Edit Subject '.$subject->id)
+                        <a href="{{ route('subjects.edit', $subject->id) }}" class="btn btn-info" role="button">Edit</a>
+                        @endcan
+                        @can('Delete Subject '.$subject->id)
+
+                          {!! Form::submit('Delete',['class'=>'btn btn-success']) !!}
+
+                        @endcan
+                        {!! Form::close() !!}
+                <!--edit and delete button end-->
+                    </div>
                 </div>
+
+                <br>
+                 </div>
             @endforeach
+
         </div>
 
         <div class="text-center">
-            {!! $subject->links() !!}
+            {!! $subjects->links() !!}
         </div>
     </div>
 </div>
-
-@can('Create ClassSubject')
-<div class="col-lg-8"></div>
-    <a href="{{ route('classSubjects.create') }}" class="btn btn-info" role="button" style="background-color: #0b9b7e;border-color: #0b9b7e;">Add Subject For a Class</a>
-@endcan
+</div>
+@endsection
